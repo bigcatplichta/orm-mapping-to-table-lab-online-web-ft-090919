1e8bfb186bdc87d5cc0aa56e32257199ab2aeb77
Matt Plichta <bigcatplichta@gmail.com>
2019-10-10 18:44:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_reader :id 
   @@all = []
   
-  def initialize(id=nil, name, grade)
+  def initialize(id=nil, name=nil, grade=nil)
     @name = name 
     @grade = grade 
     @@all << self
@@ -43,11 +43,16 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
+  # def self.create(name:, grade:)
+  #   student = Student.new(name, grade)
+  #   student.save
+  #   student
+  # end
+  
+  def self.create(student_info)
+    student = Student.new 
+    student_info.each {|k, v| student.send(k=,v)
     student.save
     student
   end
-  
-  
 end
