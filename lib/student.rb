c09b6f8c3b83c2b1952f1ad85db411d61655d6b6
Matt Plichta <bigcatplichta@gmail.com>
2019-10-10 18:16:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,23 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_accessor
+  attr_accessor :name, :age 
+  attr_reader :id 
+  @@all = []
   
+  def initialize(id=nil, name, age)
+    @name = name 
+    @age = age 
+    @@all << self
+  end 
+  
+  def self.create_table
+    sql = <<- SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER 
+        );
+      SQL 
+  end 
 end
